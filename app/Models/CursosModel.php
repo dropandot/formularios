a3ca00ef1afbfdeca6ebdb1b8d99a0db97a5f6dd
HotaruV <faotqm@hotmail.com>
2022-04-29 12:12:01 -0500

    Request

--- app/Models/CursosModel.php
+++ app/Models/CursosModel.php
@@ -9,6 +9,7 @@ class CursosModel extends Mysql{
     public function InsertarCursoSemestral($c1, $c2, $c3){
        $sql = "Insert tabla c1, c2, c3into values(?,?,?)";
         $request = $this->insert($sql, [$c1,$c2,$c3]);
+        return $request;
     }
 
 
