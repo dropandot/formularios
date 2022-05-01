b472e7b7d4632eae68a745903d98f16f3948e7fd
TeresitaReyna <teresitareyna.epsanol@hotmail.com>
2022-05-01 02:57:16 -0500

    Agrefando html

--- Views/Cursos/semestral.php
+++ Views/Cursos/semestral.php
@@ -1,5 +1,5 @@
 <?php include_once 'Views/templates/head.php' ?>
 <!-- probando cursos semestral -->
 <?php include_once 'Views/formulario.php' ?>
-
+<?php include_once 'Views/informacion.php' ?>
 <?php include_once 'Views/templates/footer.php' ?>
