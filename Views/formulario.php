cc0f17dfaaf96253d676b6279b4f485df3244d6a
TeresitaReyna <teresitareyna.epsanol@hotmail.com>
2022-05-01 02:22:10 -0500

    Arreglando el formulario

--- Views/formulario.php
+++ Views/formulario.php
@@ -1,5 +1,11 @@
-<section class="form">
-    <?php foreach($data['cursos'] as $curso) : ?>
-        <option value="<?= $curso->id ?>"> <?= $curso->nombre_curso ?></option>
-    <?php endforeach ?>
-</section>
+
+<select name="cursos" id="cursos">
+    <option value="0">--Seleccione un curso</option>
+<?php foreach($data['cursos'] as $curso) : ?>
+    <option value="<?= $curso->id ?>"> <?= $curso->nombre_curso ?></option>
+
+ 
+<?php endforeach ?>
+
+
+</select>
