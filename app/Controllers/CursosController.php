15c717407d9887b8b8728247351df40b5cdc56b4
Bryant Israel Ponce Palomo <bip_israel@live.com>
2022-05-01 23:10:09 -0500

    funcion insertar cursos

--- app/Controllers/CursosController.php
+++ app/Controllers/CursosController.php
@@ -58,17 +58,39 @@ class cursos extends Controllers
             'cabecera' => 'Curso Anual',
             'tag_pages' => 'Curso Thinkers Anual'
         ];
-        $seperadores = explode('/', $_GET['url']);
-        $anual = 1;
-        Utils::dd($seperadores[1]);
-        echo("2");
-        if(!empty($_POST)){
-            $prueba = $_POST['prueba'];
-            $prueba1 = $_POST['prueba1'];
-            $prueba2 = $_POST['prueba2'];
-            $insertar = $this->model->InsertarCursoSemestral($prueba, $prueba1, $prueba2);
-        }
         $this->views->getView($this, 'anual', $data);
+        if(!empty($_POST)){
+            $id_universidad=$_POST['form_uni'];
+            $dp_nombre=$_POST['form_name'];
+            $dp_edad=$_POST['form_edad'];
+            $dp_whatsApp= $_POST['form_num'];
+            $dp_email=$_POST['form_email'];
+            $dp_facebook= $_POST['form_face'];
+            $dp_domicilio=$_POST['form_dom'];
+            $data_carrera= $_POST['form_carrera'];
+            $data_escuela= $_POST['form_escuela'];
+            $data_horario= $_POST['form_horario'];
+            $data_razonIngreso= $_POST['form_cuentanos'];
+            $ref_saberNosotros= $_POST['form_csdt']; 
+            $ref_nombreTutor= $_POST['form_name_padre'];
+            $ref_telefonoTutor= $_POST['form_tel_padre'];
+            $id_curso = 2;
+            $ingresar = $this->model->InsertarIngresos($id_universidad, $dp_nombre, $dp_edad, $dp_whatsApp, $dp_email, $dp_facebook, $dp_domicilio, $data_carrera,
+            $data_escuela, $data_horario, $data_razonIngreso, $ref_saberNosotros, $ref_nombreTutor, $ref_telefonoTutor, $id_curso);
+            if($ingresar){
+                ?>
+                    echo'<script type="text/javascript">
+                    alert("registro completo");
+                    </script>';
+                <?php
+            }else{
+                ?>
+                    echo'<script type="text/javascript">
+                    alert("registro incompleto");
+                    </script>';
+                <?php
+            }
+        }
     }
 
     public function semiIntensivo()
@@ -77,9 +99,37 @@ class cursos extends Controllers
             'cabecera' => '',
             'tag_pages' => 'Curso Thinkers Semi intensivo'
         ];
-        $this->views->getView($this, 'semi_intensivo', $data);
-        if(isset($_POST['register'])){
-                echo "hola si jala esto";
+        if(!empty($_POST)){
+            $id_universidad=$_POST['form_uni'];
+            $dp_nombre=$_POST['form_name'];
+            $dp_edad=$_POST['form_edad'];
+            $dp_whatsApp= $_POST['form_num'];
+            $dp_email=$_POST['form_email'];
+            $dp_facebook= $_POST['form_face'];
+            $dp_domicilio=$_POST['form_dom'];
+            $data_carrera= $_POST['form_carrera'];
+            $data_escuela= $_POST['form_escuela'];
+            $data_horario= $_POST['form_horario'];
+            $data_razonIngreso= $_POST['form_cuentanos'];
+            $ref_saberNosotros= $_POST['form_csdt']; 
+            $ref_nombreTutor= $_POST['form_name_padre'];
+            $ref_telefonoTutor= $_POST['form_tel_padre'];
+            $id_curso = 3;
+            $ingresar = $this->model->InsertarIngresos($id_universidad, $dp_nombre, $dp_edad, $dp_whatsApp, $dp_email, $dp_facebook, $dp_domicilio, $data_carrera,
+            $data_escuela, $data_horario, $data_razonIngreso, $ref_saberNosotros, $ref_nombreTutor, $ref_telefonoTutor, $id_curso);
+            if($ingresar){
+                ?>
+                    echo'<script type="text/javascript">
+                    alert("registro completo");
+                    </script>';
+                <?php
+            }else{
+                ?>
+                    echo'<script type="text/javascript">
+                    alert("registro incompleto");
+                    </script>';
+                <?php
+            }
         }
     }
     
