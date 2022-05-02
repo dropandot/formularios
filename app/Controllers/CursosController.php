acfc154b4b67ff9b97df80c065aebbbbd1b555a7
Bryant Israel Ponce Palomo <bip_israel@live.com>
2022-05-01 22:59:29 -0500

    limpieza

--- app/Controllers/CursosController.php
+++ app/Controllers/CursosController.php
@@ -2,13 +2,10 @@
 
 class cursos extends Controllers
 {
-
     public function __construct()
     {
         parent::__construct();
     }
-
-
 
     public function semestral()
     {
@@ -18,9 +15,7 @@ class cursos extends Controllers
             'tag_pages' => 'Curso Thinkers Semestral'
         ];
         $this->views->getView($this, 'semestral', $data);
-        
-        // echo("1");
-        
+
         if(!empty($_POST)){
             $id_universidad=$_POST['form_uni'];
             $dp_nombre=$_POST['form_name'];
@@ -39,19 +34,20 @@ class cursos extends Controllers
             $id_curso = 1;
             $ingresar = $this->model->InsertarIngresos($id_universidad, $dp_nombre, $dp_edad, $dp_whatsApp, $dp_email, $dp_facebook, $dp_domicilio, $data_carrera,
             $data_escuela, $data_horario, $data_razonIngreso, $ref_saberNosotros, $ref_nombreTutor, $ref_telefonoTutor, $id_curso);
-                // $ingresar = new CursosModel();
-                // $ingresar = $this->model->InsertarIngresos($dp_nombre, $dp_edad, $dp_whatsApp, $dp_email, $dp_facebook, $dp_domicilio, $data_carrera,
-                // $data_escuela, $data_horario, $data_razonIngreso, $ref_saberNosotros, $ref_nombreTutor, $ref_telefonoTutor);
-                // InsertarCursoSemestral($prueba, $prueba1, $prueba2);
             if($ingresar){
                 ?>
                     echo'<script type="text/javascript">
                     alert("registro completo");
                     </script>';
                 <?php
+            }else{
+                ?>
+                    echo'<script type="text/javascript">
+                    alert("registro incompleto");
+                    </script>';
+                <?php
             }
         }
-
 
         $this->views->getView($this, 'semestral', $data);
     }
@@ -65,12 +61,7 @@ class cursos extends Controllers
         $seperadores = explode('/', $_GET['url']);
         $anual = 1;
         Utils::dd($seperadores[1]);
-
-        // $seperadores = explode('/', $_GET['url']);
-        // $anual = 1;
-        // Utils::dd($seperadores[1]);
         echo("2");
-        //echo Utils::dd($_GET);
         if(!empty($_POST)){
             $prueba = $_POST['prueba'];
             $prueba1 = $_POST['prueba1'];
@@ -80,7 +71,6 @@ class cursos extends Controllers
         $this->views->getView($this, 'anual', $data);
     }
 
-
     public function semiIntensivo()
     {
         $data = [
@@ -89,9 +79,7 @@ class cursos extends Controllers
         ];
         $this->views->getView($this, 'semi_intensivo', $data);
         if(isset($_POST['register'])){
-            // if(!empty($_POST)){
                 echo "hola si jala esto";
-            // }
         }
     }
     
